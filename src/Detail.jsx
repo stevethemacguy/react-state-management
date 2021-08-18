e0a91449eed971ed0756a69e19ac395d0453647c
Steven Dunn <Dunn.Steven1@gmail.com>
2021-08-17 19:04:12 -0700

    Clicking an 'Add to Cart' button on the detail page now re-directs the user to the 'cart' route.

--- src/Detail.jsx
+++ src/Detail.jsx
@@ -1,16 +1,16 @@
 import React from "react";
-import {useParams} from 'react-router-dom';
+import {useNavigate, useParams} from 'react-router-dom';
 import useFetch from './services/useFetch';
 import Spinner from './Spinner';
 import PageNotFound from './PageNotFound';
 
 export default function Detail() {
   const {id} = useParams(); // Note: 'id' is the name we used when setting up the route param (e.g. shoes/id)
+  const navigate = useNavigate(); // Used to tell the router to navigate to a different route.
 
   // Destructure the object returned by useFetch, which includes the data (i.e. product), a T/F loading value, and (possibly) an error
   // The 'data: product' syntax renames the data to product. It removes the need for an extra variable (i.e. const product = data).
   const {data: product, loading, error} = useFetch(`products/${id}`);
-
 
   if (loading) return <Spinner />;
   // CAUTION: This line must come *before* the error line below. If it comes after, then the error will be thrown instead of showing the 404 page.
@@ -22,6 +22,9 @@ export default function Detail() {
       <h1>{product.name}</h1>
       <p>{product.description}</p>
       <p id="price">${product.price}</p>
+      <div>
+        <button className="btn btn-primary" onClick={() => {navigate('/cart')}}>Add to Cart</button>
+      </div>
       <img src={`/images/${product.image}`} alt={product.category} />
     </div>
   );
