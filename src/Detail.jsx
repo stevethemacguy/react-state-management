27213930beaf7c3ea96923b95b39beb165b94b98
Steven Dunn <Dunn.Steven1@gmail.com>
2021-08-17 18:58:36 -0700

    The 404 page is now shown if the user enters an invalid product ID.

--- src/Detail.jsx
+++ src/Detail.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import {useParams} from 'react-router-dom';
 import useFetch from './services/useFetch';
 import Spinner from './Spinner';
+import PageNotFound from './PageNotFound';
 
 export default function Detail() {
   const {id} = useParams(); // Note: 'id' is the name we used when setting up the route param (e.g. shoes/id)
@@ -10,10 +11,12 @@ export default function Detail() {
   // The 'data: product' syntax renames the data to product. It removes the need for an extra variable (i.e. const product = data).
   const {data: product, loading, error} = useFetch(`products/${id}`);
 
-  if (error) throw error;
-  if (loading) return <Spinner />;
 
-  // TODO: Display these products details
+  if (loading) return <Spinner />;
+  // CAUTION: This line must come *before* the error line below. If it comes after, then the error will be thrown instead of showing the 404 page.
+  if (!product) return <PageNotFound/>;
+  if (error) throw error;
+
   return (
     <div id="detail">
       <h1>{product.name}</h1>
@@ -22,5 +25,4 @@ export default function Detail() {
       <img src={`/images/${product.image}`} alt={product.category} />
     </div>
   );
-
 }
