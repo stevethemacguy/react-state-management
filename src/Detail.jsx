91fdac3f90b3e42c5a28e4d1545663fe4e529c30
Steven Dunn <Dunn.Steven1@gmail.com>
2021-08-17 18:47:56 -0700

    The detail page now reads the 'id' route param from the URL and uses it to retrieve the associated product from the DB.

--- src/Detail.jsx
+++ src/Detail.jsx
@@ -1,15 +1,26 @@
 import React from "react";
+import {useParams} from 'react-router-dom';
+import useFetch from './services/useFetch';
+import Spinner from './Spinner';
 
 export default function Detail() {
-  return <h1>Detail</h1>;
+  const {id} = useParams(); // Note: 'id' is the name we used when setting up the route param (e.g. shoes/id)
+
+  // Destructure the object returned by useFetch, which includes the data (i.e. product), a T/F loading value, and (possibly) an error
+  // The 'data: product' syntax renames the data to product. It removes the need for an extra variable (i.e. const product = data).
+  const {data: product, loading, error} = useFetch(`products/${id}`);
+
+  if (error) throw error;
+  if (loading) return <Spinner />;
 
   // TODO: Display these products details
-  // return (
-  //   <div id="detail">
-  //     <h1>{product.name}</h1>
-  //     <p>{product.description}</p>
-  //     <p id="price">${product.price}</p>
-  //     <img src={`/images/${product.image}`} alt={product.category} />
-  //   </div>
-  // );
+  return (
+    <div id="detail">
+      <h1>{product.name}</h1>
+      <p>{product.description}</p>
+      <p id="price">${product.price}</p>
+      <img src={`/images/${product.image}`} alt={product.category} />
+    </div>
+  );
+
 }
--- src/services/productService.js
+++
@@ -1,5 +0,0 @@
-export async function getProduct(id) {
-  const response = await fetch(baseUrl + "products/" + id);
-  if (response.ok) return response.json();
-  throw response;
-}
