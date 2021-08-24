0de30b0b8689683ff095d468103515d8e2ef730f
Steven Dunn <Dunn.Steven1@gmail.com>
2021-08-23 19:07:50 -0700

    Added a formErrors object to hold all of the errors in the form. The keys of the object match the input element IDs. For example, if the 'city' is missing, then formErrors['city'] will return an error message. We use the formErrors object to show all the errors to the user at the top of the page.

--- src/Checkout.jsx
+++ src/Checkout.jsx
@@ -5,11 +5,11 @@ import Spinner from './Spinner';
 const STATUS = {
   IDLE: 'IDLE',
   SUBMITTING: 'SUBMITTING',
-  SUBMITTED: 'SUBMITTED',
+  INVALID: 'INVALID', // The status is INVALID when the user attempts to submit, but the AJAX request failed or did not complete
   COMPLETED: 'COMPLETED'
 }
 
-// Declaring outside component to avoid recreation on each render
+// Declared outside component to avoid recreation on each render
 const emptyAddress = {
   city: '',
   country: '',
@@ -19,6 +19,13 @@ export default function Checkout({cart, emptyCart}) {
   const [address, setAddress] = useState(emptyAddress);
   const [status, setStatus] = useState(STATUS.IDLE);
   const [saveError, setSaveError] = useState(null);
+
+  // FormErrors is an object that holds all the address errors in the form. The object uses the <input> IDs as keys (i.e. property names)
+  // and the error messages as values. For example, if the 'city' input has an error, then formErrors['city'] will return 'City is required'.
+  // FormErrors is used to show all the errors at the top of the page.
+  const formErrors = getFormErrors(address);
+  // If the errors object is empty (i.e. has no keys), then there are no errors.
+  const isValid = Object.keys(formErrors).length === 0;
 
   function handleChange(e) {
     e.persist(); // Note: No longer required as of React 17
@@ -35,20 +42,36 @@ export default function Checkout({cart, emptyCart}) {
     // TODO
   }
 
+  // Returns an empty object if there are no errors. Otherwise, returns an object containing all the errors
+  function getFormErrors(address) {
+    const errors = {};
+    // If the city input doesn't have a value, push the error message string onto the errors object using 'city' as the property name/key
+    if (!address.city) {
+      errors.city = 'City is required';
+    }
+    if (!address.country) {
+      errors.country = 'Country is required';
+    }
+    return errors;
+  }
+
   async function handleSubmit(event) {
     event.preventDefault(); // Perform our own validation instead of submitting.
     setStatus(STATUS.SUBMITTING);
-    try {
-      await saveShippingAddress(address);
-      emptyCart();
-      setStatus(STATUS.COMPLETED);
-    } catch (e) {
-      setSaveError(e);
+    // Only make the request if form is valid.
+    if (isValid) {
+      try {
+        await saveShippingAddress(address);
+        emptyCart();
+        setStatus(STATUS.COMPLETED);
+      } catch (e) {
+        setSaveError(e);
+      }
     }
-    // finally
-    // {
-    //   setStatus(STATUS.SUBMITTED);
-    // }
+    // The form is not valid, so change the status
+    else {
+      setStatus(STATUS.INVALID);
+    }
   }
 
   if (saveError) throw saveError;
@@ -59,6 +82,24 @@ export default function Checkout({cart, emptyCart}) {
 
   return (
     <>
+      {
+        // If the form is invalid AND the user tried to submit it, display a list of errors above the form
+        (!isValid && status === STATUS.INVALID) && (
+          <div className="alert">
+            <p className="alert">Please fix the following errors</p>
+            <ul>
+              {
+                // The formErrors object uses the actual property name as a key (e.g. city, country)
+                // so Object.keys() is used to return an array containing each property (i.e. error) in the formErrors object.
+                // We then map that array to create an <li> for each error, using the error key name to access the error message.
+                Object.keys(formErrors).map((errorKey) => {
+                  return <li key={errorKey}>{formErrors[errorKey]}</li>
+                })
+              }
+            </ul>
+          </div>
+        )
+      }
       <h1>Shipping Info</h1>
       <form onSubmit={handleSubmit}>
         <div>
@@ -66,7 +107,6 @@ export default function Checkout({cart, emptyCart}) {
           <br/>
           <input id="city" type="text" value={address.city} onBlur={handleBlur} onChange={handleChange}/>
         </div>
-
         <div>
           <label htmlFor="country">Country</label>
           <br/>
@@ -77,7 +117,6 @@ export default function Checkout({cart, emptyCart}) {
             <option value="USA">USA</option>
           </select>
         </div>
-
         <div>
           <input disabled={status === STATUS.SUBMITTING} type="submit" className="btn btn-primary" value="Save Shipping Info"/>
         </div>
